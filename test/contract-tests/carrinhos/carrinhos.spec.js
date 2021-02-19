e7fcf466704368721bfd46b04195fa0c9b20e0d6
Lucas Fraga <lucass.fragaa@gmail.com>
2021-02-19 00:28:18 -0300

    testando yaml - 16

--- test/contract-tests/carrinhos/carrinhos.spec.js
+++ test/contract-tests/carrinhos/carrinhos.spec.js
@@ -1,9 +1,9 @@
-const APP_URL = process.env.APP_URL;
+const url = process.env.APP_URL;
 const carrinhos = require("../../utils/schemas/carrinhos-schema");
 
 describe("[CONTRATO] :: CARRINHOS - teste de contrato", () => {
   it("/GET deve validar o contrato (schema) do endpoint de carrinhos", async () => {
-    await request(APP_URL)
+    await request(url)
       .get("/carrinhos")
       .timeout({ response: DEFAULT_TIMEOUT, deadline: DEFAULT_TIMEOUT })
       .then((result) => {
--- test/contract-tests/produtos/produtos.spec.js
+++ test/contract-tests/produtos/produtos.spec.js
@@ -1,9 +1,9 @@
-const APP_URL = process.env.APP_URL;
+const url = process.env.APP_URL;
 const produtos = require("../../utils/schemas/produtos-schema");
 
 describe("[CONTRATO] :: PRODUTOS - teste de contrato", () => {
   it("/GET deve validar o contrato (schema) do endpoint de produtos", async () => {
-    await request(APP_URL)
+    await request(url)
       .get("/produtos")
       .timeout({ response: DEFAULT_TIMEOUT, deadline: DEFAULT_TIMEOUT })
       .then((result) => {
